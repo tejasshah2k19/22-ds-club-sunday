a0269101be33ea5b40e9fdfa076af450c3b6778b
tejas <tejasshah@gmail.com>
2022-04-10 13:29:31 +0530

    Stack

---
+++ stack.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#define SIZE 5
+
+int stack[SIZE];
+int top=-1;
+
+void push(){
+    int num;
+
+    if(top == SIZE - 1)
+        printf("\nStackOverFlow");
+    else{
+        printf("\nEnter number");
+        scanf("%d",&num);
+        top++;
+        stack[top] = num ;
+    }
+}
+void pop(){
+    int num;
+    if(top == - 1)
+        printf("\nStackUnderFlow");
+    else{
+        num = stack[top];
+        printf("\n%d pop",num);
+        top--;
+    }
+}
+void display(){
+    int i;
+
+    if(top == -1)
+        printf("\nStackUnderFlow");
+    else{
+        for(i=top;i>=0;i--){
+            printf("\n%d",stack[i]);//
+        }
+    }
+}
+
+int main(){
+    int choice;
+
+    while(1){
+        printf("\n0 For Exit\n1 For Push\n2 For pop\n3 For Display");
+        printf("\nEnter Choice");
+        scanf("%d",&choice);
+
+        switch(choice){
+            case 0:
+                exit(0);
+            case 1:
+                push();break;
+            case 2:
+                pop();break;
+            case 3:
+                display();break;
+            default:
+                printf("\nInvalid Choice PTA");
+        }
+    }
+
+    return 0;
+
+}
