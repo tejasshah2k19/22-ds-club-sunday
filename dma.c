875ee56475dbdd167e98f4636e4d07028bf33ee2
tejas <tejasshah@gmail.com>
2022-05-29 13:32:19 +0530

    dma

--- dma.c
+++ dma.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 
+struct std {
+    char name[10];//10
+    int rollNum; //4
+    float resut;//4
+    int m,s,e;//12 [ 16 ]
+};
+
 int main()
 {
     int *p,i,x=3;
 
     //
     p = (int*)malloc(sizeof(int)*x);
+    //p => 50 byte --> 16 int , 10 char , 4  float
     //p = (int*)calloc(5,sizeof(int));
 
     for(i=0;i<x;i++){
