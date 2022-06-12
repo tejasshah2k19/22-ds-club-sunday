f5f76ace81dae4e16a2f87aba29da42aa8a1d6e7
tejas <tejasshah@gmail.com>
2022-06-12 13:18:31 +0530

    insertatanybefore deletebeg deleteany

--- linkedList1.c
+++ linkedList1.c
@@ -6,11 +6,17 @@ struct node{
 }*head=NULL;
 
 int main(){
-    int ch;
+    int ch,num;
+    insert(10);
+    insert(20);
+    insert(30);
+    insert(40);
+    insert(50);
 
     while(1){
         printf("\n0 for exit\n1  for insert\n2 for display\n3 for delete ");
-        printf("\n4 for insertAtBeg\n5 for insertAtAnyAfter\n");
+        printf("\n4 for insertAtBeg\n5 for insertAtAnyAfter");
+        printf("\n6 for insertATAnyBefore\n7 for deleteBeg\n8 for Delete Any");
         printf("\nEnter choice");
         scanf("%d",&ch);
 
@@ -18,27 +24,32 @@ int main(){
             case 0:
                 exit(0);
             case 1:
-                insert();break;
+                printf("\nEnter number");
+                scanf("%d",&num);//10 20 30 40
+                insert(num);break; //insert at last
             case 2:
-                display();break;
+                display();break;// beg to end display
             case 3:
-                deleteNode();break;
+                deleteNode();break; // delete last
             case 4:
-                insertAtBeg();break;
+                insertAtBeg();break; // insert at beg
             case 5:
-                insertAtAnyAfter();break;
+                insertAtAnyAfter();break; //insert at any after
+            case 6:
+                insertAtAnyBefore();break; //insert at any before
+            case 7:
+                deleteNodeBeg();break;
+            case 8:
+                deleteAny();break;
         }
     }
 }
 
 
-void insert(){
+void insert(int num){
 
-    int num;
     struct node *tmp,*p;
 
-    printf("\nEnter number");
-    scanf("%d",&num);//10 20 30 40
     if(head == NULL){
         head  = (struct node*)malloc(sizeof(struct node));
         head->data=num;
@@ -145,15 +156,84 @@ void insertAtAnyAfter(){
 
 }
 
+void insertAtAnyBefore(){
+    int search,num,isFound=0;
+    struct node *p,*q,*tmp;
+
+    if(head == NULL){
+        printf("\nEnter Few DAta");
+    }else{
+        printf("\nEnter Source Node : before ");
+        scanf("%d",&search);
+
+        p = head;
+        q = head;
+        while(p!=NULL){
+            if(p->data == search){
+                isFound = 1;
+                break;
+            }
+            q = p;
+            p=p->next;
+        }
+
+        if(isFound){
+            printf("\nEnter new data");
+            scanf("%d",&num);
+
+            tmp = (struct node*)malloc(sizeof(struct node));
+            tmp->data = num;
+
+            tmp->next = p;
+            q->next = tmp;
+        }else{
+            printf("\nInvalid Source Data PTA");
+        }
+    }
+}
 
 
+void deleteNodeBeg(){
+
+    struct node *p;
+    p=head;
+    head = head->next;
+    free(p);
+}
 
 
+void deleteAny(){
 
+    struct node *p,*q;
+    int num,isFound =0;
 
+    printf("\nEnter num to delete");
+    scanf("%d",&num);
+    p=head;
+    q=head;
+    while(p!=NULL){
+        if(p->data == num){
+            isFound = 1;
+            break;
+        }
+        q=p;
+        p=p->next;
+    }
 
+    if(isFound){
+        if(p==head){
+            deleteNodeBeg();
+        } else if(p->next == NULL){
+                deleteNode();
+        } else{
+            q->next = p->next;
+            free(p);
+        }
+    }else{
+        printf("\nInvalid Source PTA");
+    }
 
-
+}
 
 
 
